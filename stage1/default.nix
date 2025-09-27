e6c6d52e4c9b7358cc3cd0d27b4d6f6c10731a4c
Kris Scott <kscott91@gmail.com>
2025-09-27 14:37:04 -0700

    Refactor stage1 pkg

--- stage1/default.nix
+++ stage1/default.nix
@@ -1,23 +1,22 @@
 {
   lib,
   stdenvNoCC,
-}: let
-  includeSrc = lib.cleanSource ../include;
-in
-  stdenvNoCC.mkDerivation {
-    pname = "prexy";
-    version = builtins.readFile ../version.txt;
-    src = lib.cleanSource ./.;
+}:
+stdenvNoCC.mkDerivation {
+  pname = "prexy";
+  version = builtins.readFile ../version.txt;
+  src = lib.cleanSource ./..;
 
-    installPhase = ''
-      mkdir -p $out/bin
-      chmod +x prexy.awk
-      mv prexy.awk prexyp $out/bin/
-      cp -r ${includeSrc} $out/include
-    '';
+  installPhase = ''
+    mkdir -p $out/share
 
-    meta = {
-      license = lib.licenses.gpl3Plus;
-      mainProgram = "prexyp";
-    };
-  }
+    mv stage1 $out/bin
+    mv include $out
+    mv docs $out/share/prexy-docs
+  '';
+
+  meta = {
+    license = lib.licenses.gpl3Plus;
+    mainProgram = "prexyp";
+  };
+}
