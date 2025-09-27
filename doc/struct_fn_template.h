6ec1fd76a32c802c7f497113aea31c5729221344
Kris Scott <kscott91@gmail.com>
2025-09-27 14:29:12 -0700

    Cleanup struct fn template

--- doc/struct_fn_template.h
+++ doc/struct_fn_template.h
@@ -1,14 +1,14 @@
-#ifndef PREXY_TEMPLATE_H_
-#define PREXY_TEMPLATE_H_
+#ifndef X_FN_H_
+#define X_FN_H_
 
 // Field implementations
 
-#define PREXY_FN_simple(type, varname)                                         \
+#define X_FN_simple(type, varname)                                             \
     {                                                                          \
         (x)->varname;                                                          \
     }
 
-#define PREXY_FN_simple_array(type, varname, len)                              \
+#define X_FN_simple_array(type, varname, len)                                  \
     {                                                                          \
         for (int i = 0; i < len; ++i)                                          \
         {                                                                      \
@@ -16,12 +16,12 @@
         }                                                                      \
     }
 
-#define PREXY_FN_struct(type, varname)                                         \
+#define X_FN_struct(type, varname)                                             \
     {                                                                          \
         (x)->varname;                                                          \
     }
 
-#define PREXY_FN_struct_array(type, varname, len)                              \
+#define X_FN_struct_array(type, varname, len)                                  \
     {                                                                          \
         for (int i = 0; i < len; ++i)                                          \
         {                                                                      \
@@ -29,12 +29,12 @@
         }                                                                      \
     }
 
-#define PREXY_FN_enum(type, varname)                                           \
+#define X_FN_enum(type, varname)                                               \
     {                                                                          \
         (x)->varname;                                                          \
     }
 
-#define PREXY_FN_enum_array(type, varname, len)                                \
+#define X_FN_enum_array(type, varname, len)                                    \
     {                                                                          \
         for (int i = 0; i < len; ++i)                                          \
         {                                                                      \
@@ -43,14 +43,14 @@
     }
 
 // Field selector
-#define PREXY_FN_FIELD_SELECT(fkind, ...) PREXY_FN_##fkind(__VA_ARGS__)
+#define X_FN_FIELD_SELECT(fkind, ...) X_FN_##fkind(__VA_ARGS__)
 
 // Function prototype
-#define prexy_fn_decl(name) void name##_fn(FILE *stream, struct name const *x)
+#define fn_decl(name) void name##_fn(FILE *stream, struct name const *x)
 
 // Implement with explicit XMacro
-#define prexy_fn_impl(name, FIELDS_X)                                          \
-    prexy_fn_decl(name) { FIELDS_X(PREXY_FN_FIELD_SELECT) }                    \
+#define fn_impl(name, FIELDS_X)                                                \
+    fn_decl(name) { FIELDS_X(X_FN_FIELD_SELECT) }                              \
     static_assert(1, "")
 
 #endif
