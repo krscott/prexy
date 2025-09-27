6e69d5adfc644f9a986773b8bdcfc7da2d022fb5
Kris Scott <kscott91@gmail.com>
2025-09-27 12:36:40 -0700

    Move prexy.h to root include dir

--- stage1/default.nix
+++ stage1/default.nix
@@ -1,20 +1,23 @@
 {
   lib,
   stdenvNoCC,
-}:
-stdenvNoCC.mkDerivation {
-  pname = "prexy";
-  version = builtins.readFile ../version.txt;
-  src = lib.cleanSource ./.;
+}: let
+  includeSrc = lib.cleanSource ../include;
+in
+  stdenvNoCC.mkDerivation {
+    pname = "prexy";
+    version = builtins.readFile ../version.txt;
+    src = lib.cleanSource ./.;
 
-  installPhase = ''
-    mkdir -p $out/bin
-    chmod +x prexy.awk
-    mv prexy.awk $out/bin/prexy
-  '';
+    installPhase = ''
+      mkdir -p $out/bin
+      chmod +x prexy.awk
+      mv prexy.awk $out/bin/prexy
+      cp -r ${includeSrc} $out/include
+    '';
 
-  meta = {
-    license = lib.licenses.gpl3Plus;
-    mainProgram = "prexy";
-  };
-}
+    meta = {
+      license = lib.licenses.gpl3Plus;
+      mainProgram = "prexy";
+    };
+  }
