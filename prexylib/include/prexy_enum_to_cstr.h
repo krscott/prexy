7f14e5cade1052c7425f757f4a23f0959b72febe
Kris Scott <kscott91@gmail.com>
2025-09-27 11:51:27 -0700

    Cleanup enum_to_cstr

--- prexylib/include/prexy_enum_to_cstr.h
+++ prexylib/include/prexy_enum_to_cstr.h
@@ -4,7 +4,7 @@
 #include "prexy_core.h"
 #include <assert.h>
 
-#define PREXY_CASE_TO_CSTR(x)                                                  \
+#define PREXY_ENUM_TO_CSTR_CASE(x)                                             \
     case x:                                                                    \
         return #x;
 
@@ -13,12 +13,12 @@ prexy_tag(prexy_enum_to_cstr);
 #define prexy_enum_to_cstr_decl(name)                                          \
     char const *name##_to_cstr(enum name const val)
 
-#define prexy_enum_to_cstr_impl(name, FIELDS_X)                                \
+#define prexy_enum_to_cstr_impl(name, VARIANTS_X)                              \
     prexy_enum_to_cstr_decl(name)                                              \
     {                                                                          \
         switch (val)                                                           \
         {                                                                      \
-            FIELDS_X(PREXY_CASE_TO_CSTR)                                       \
+            VARIANTS_X(PREXY_ENUM_TO_CSTR_CASE)                                \
                                                                                \
         default:                                                               \
             assert(0 && "Invalid enum " #name " value");                       \
